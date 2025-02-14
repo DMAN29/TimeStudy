b8d67c478f560f3c23383b20a0afdc80585aa55d
DMAN29 <ranjan.dr29@gmail.com>
2025-02-14 18:13:31 +0530

    change

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_BASE_URL: "https://api.surgot.in/", // Change this URL when needed
+    API_BASE_URL: "https://api.surgot.in", // Change this URL when needed
 };
   
 export default config;
