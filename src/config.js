ed590e6121e0684f8ebc8168891faa9e7d3656b0
DMAN29 <ranjan.dr29@gmail.com>
2025-02-14 15:49:15 +0530

    vercel fix

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_BASE_URL: "http://116.203.204.42:5000", // Change this URL when needed
+    API_BASE_URL: "", // Change this URL when needed
   };
   
   export default config;
